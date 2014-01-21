ef2c3ccf46d89ef164d793b718c408a98266f4b7
PockyMaster <tsaok@uw.edu>
2014-01-20 20:12:35 -0800

    starting chat room files
    Added blank-ish chat room files (xml, java)

--- gen/com/example/digitaltable/R.java
+++ gen/com/example/digitaltable/R.java
@@ -50,8 +50,9 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int match_making=0x7f030001;
-        public static final int profile=0x7f030002;
+        public static final int chat_room=0x7f030001;
+        public static final int match_making=0x7f030002;
+        public static final int profile=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
